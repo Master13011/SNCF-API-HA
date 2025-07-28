cef33a479e8a9390c6f5397e619ce0f4a3d568a3
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-07-28 09:55:55 +0200

    Update const.py

--- custom_components/sncf_trains/const.py
+++ custom_components/sncf_trains/const.py
@@ -1,8 +1,5 @@
 DOMAIN = "sncf_trains"
-CONF_API_KEY = "api_key"
-CONF_STATION = "station"
-CONF_FROM_HOUR = "from_hour"
-CONF_TO_HOUR = "to_hour"
-SNCF_API_BASE = "https://api.sncf.com/v1/coverage/sncf"
-
-PLATFORM = "sensor"
+CONF_TOKEN = "api_token"
+CONF_GARE = "station_query"
+CONF_START = "time_start"
+CONF_END = "time_end"
