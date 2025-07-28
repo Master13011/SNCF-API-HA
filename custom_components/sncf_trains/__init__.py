511837480e920bbf1cf0be11c21179f93eb1bcf8
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-07-28 09:49:08 +0200

    Update __init__.py

--- custom_components/sncf_trains/__init__.py
+++ custom_components/sncf_trains/__init__.py
@@ -1,1 +1,23 @@
-DOMAIN = "sncf_trains"
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up from configuration.yaml (not used)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up SNCF Trains from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
