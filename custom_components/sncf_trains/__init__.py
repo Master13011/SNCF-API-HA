791e49a88b9f929bb147960b3fd3e7031b41b0c6
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-07-29 11:48:29 +0200

    Update __init__.py

--- custom_components/sncf_trains/__init__.py
+++ custom_components/sncf_trains/__init__.py
@@ -1,8 +1,6 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-from .const import DOMAIN
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
