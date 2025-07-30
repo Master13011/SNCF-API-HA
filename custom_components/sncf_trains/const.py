933e7c45a62dd1e2a4756df1d2c18bebd3ad5c19
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-07-30 22:49:30 +0200

    Update const.py

--- custom_components/sncf_trains/const.py
+++ custom_components/sncf_trains/const.py
@@ -8,4 +8,7 @@ CONF_TIME_START = "time_start"
 CONF_TIME_END = "time_end"
 PLATFORMS = ["sensor"]
 
+DEFAULT_UPDATE_INTERVAL = 2  # minutes
+DEFAULT_OUTSIDE_INTERVAL = 60  # minutes
+
 ATTRIBUTION = "Data provided by api.sncf.com"
