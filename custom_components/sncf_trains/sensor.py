7a8762af82cc69a5737d8543c7377a6109effda1
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-08-25 18:16:45 +0200

    Update sensor.py

--- custom_components/sncf_trains/sensor.py
+++ custom_components/sncf_trains/sensor.py
@@ -7,6 +7,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.device_registry import DeviceEntryType
 from typing import Any, cast
 
 from .const import (
